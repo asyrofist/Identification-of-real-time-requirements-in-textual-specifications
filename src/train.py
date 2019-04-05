8c522bac3711ce525574ea5cfd33ae65b7fd4503
wangqianwq2016 <wqan1007@gmail.com>
2019-04-05 17:25:45 +0800

    updated train.py

--- src/train.py
+++ src/train.py
@@ -3,11 +3,12 @@ from src.utils.api import *
 from src.utils.database import Database
 from src.utils.TextPreProcessor import TextPreProcessor
 import time
+import numpy as np
 
 start_time = time.time()
 SIZE = 5000
 EMBEDDING_TYPE = 'default'
-FEATURE_MODE = 'word2vec'  # or 'tfidf', 'word2vec'
+FEATURE_MODE = 'tfidf'  # or 'tfidf', 'word2vec'
 DOC_LIST = (1, 2, 4, 7, 11, 16, 19, 20, 21, 22, 25, 26, 30, 31, 32, 37, 39)
 # DOC_LIST = (25,)
 STOP_WORDS = TextPreProcessor.get_default_stop_words()
@@ -28,10 +29,15 @@ for dtype in DIVISION[1]:
 print('Positive sentences ', len(pos_sentences))
 print('Negative sentences ', len(neg_sentences))
 
-Database.dump_example(pos_sentences, neg_sentences, STOP_WORDS, EMBEDDING_TYPE)
+#Database.dump_example(pos_sentences, neg_sentences, STOP_WORDS, EMBEDDING_TYPE)
 pos, neg = Database.load_example(FEATURE_MODE)
 pos, neg = change_feature_ratio(list(pos), list(neg), RATIO, mode=CHANGE_RATIO_MODE)
 print('Feature fetched! %ss' % (time.time() - start_time))
 print('pos ', len(pos))
 print('neg ', len(neg))
 
+model = NBModel(name='test',author='wang')
+data = [(np.array(sentence).reshape(-1),1,"",1) for sentence in pos]
+data += [(np.array(sentence).reshape(-1),1,"",0) for sentence in neg]
+model.train(data)
+model.evaluate(data)
