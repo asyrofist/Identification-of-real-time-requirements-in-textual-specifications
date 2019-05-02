9b16c437fe2c520ca59ec795d36fe7224b69eb45
yiranyyu <2606375857@qq.com>
2019-05-02 17:55:46 +0800

    create entrance of training: src/run.py

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-work_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.extend([work_dir])
-print(work_dir)
---
+++ src/run.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+work_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.extend([work_dir])
+print(work_dir)
+import src.train
